fefc95bbcdee0cdc51072906ad837fdeb89d7648
unknown <t_davani@yahoo.com>
2021-03-03 20:27:48 -0500

    react-youtube-app

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import VideoCard from './VideoCard'
+import './RecommendedVideos.css'
+
+export default function RecommendedVideos() {
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Parnian"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+          channel="Parnian"
+          image="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+        />
+        <VideoCard
+          title="Parnian"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+          channel="Parnian"
+          image="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+        />
+        <VideoCard
+          title="Parnian"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+          channel="Parnian"
+          image="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+        />
+        <VideoCard
+          title="Parnian"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+          channel="Parnian"
+          image="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+        />
+        <VideoCard
+          title="Parnian"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+          channel="Parnian"
+          image="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+        />
+        <VideoCard
+          title="Parnian"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+          channel="Parnian"
+          image="https://cdn.pixabay.com/photo/2020/12/27/12/07/sunrise-5863751_1280.png"
+        />
+      </div>
+    </div>
+  )
+}
